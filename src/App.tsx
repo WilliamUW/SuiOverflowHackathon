7e050eb3edd22f71f19880bb5ac416a07ff55ecb
William Wang <bwilliamwang@gmail.com>
2025-05-21 13:18:12 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,267 @@
+import suietLogo from "./assets/suiet-logo.svg";
+import "./App.css";
+import {
+  ConnectButton,
+  useAccountBalance,
+  useWallet,
+  SuiChainId,
+  ErrorCode,
+  formatSUI,
+  useSuiClient,
+} from "@suiet/wallet-kit";
+import "@suiet/wallet-kit/style.css";
+import { Transaction } from "@mysten/sui/transactions";
+import { useMemo } from "react";
+import { Ed25519PublicKey } from "@mysten/sui/keypairs/ed25519";
+import { Buffer } from "buffer";
+
+const sampleNft = new Map([
+  [
+    "sui:devnet",
+    "0xe146dbd6d33d7227700328a9421c58ed34546f998acdc42a1d05b4818b49faa2::nft::mint",
+  ],
+  [
+    "sui:testnet",
+    "0x5ea6aafe995ce6506f07335a40942024106a57f6311cb341239abf2c3ac7b82f::nft::mint",
+  ],
+  [
+    "sui:mainnet",
+    "0x5b45da03d42b064f5e051741b6fed3b29eb817c7923b83b92f37a1d2abf4fbab::nft::mint",
+  ],
+]);
+
+function createMintNftTxb(contractAddress: string) {
+  const tx = new Transaction();
+  tx.moveCall({
+    target: contractAddress,
+    arguments: [
+      tx.pure.string("Suiet NFT"),
+      tx.pure.string("Suiet Sample NFT"),
+      tx.pure.string(
+        "https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4"
+      ),
+    ],
+  });
+  return tx;
+}
+
+function App() {
+  const wallet = useWallet();
+  const client = useSuiClient();
+
+  const { balance } = useAccountBalance();
+  const nftContractAddr = useMemo(() => {
+    if (!wallet.chain) return "";
+    return sampleNft.get(wallet.chain.id) ?? "";
+  }, [wallet]);
+
+  function uint8arrayToHex(value: Uint8Array | undefined) {
+    if (!value) return "";
+    // @ts-ignore
+    return value.toString("hex");
+  }
+
+  async function handleSignAndExecuteTransaction(
+    target: string | undefined,
+    opts?: {
+      isCustomExecution?: boolean;
+    }
+  ) {
+    if (!target) return;
+    try {
+      const tx = createMintNftTxb(target);
+
+      if (!opts?.isCustomExecution) {
+        const resData = await wallet.signAndExecuteTransaction({
+          transaction: tx,
+        });
+        console.log("signAndExecuteTransaction success", resData);
+      } else {
+        const resData = await wallet.signAndExecuteTransaction(
+          {
+            transaction: tx,
+          },
+          {
+            execute: async ({ bytes, signature }) => {
+              return await client.executeTransactionBlock({
+                transactionBlock: bytes,
+                signature: signature,
+                options: {
+                  showRawEffects: true,
+                  showObjectChanges: true,
+                },
+              });
+            },
+          }
+        );
+        console.log("signAndExecuteTransaction success", resData);
+      }
+
+      alert("executeTransactionBlock succeeded (see response in the console)");
+    } catch (e) {
+      console.error("executeMoveCall failed", e);
+      alert("executeTransactionBlock failed (see response in the console)");
+    }
+  }
+
+  async function handleSignMsg() {
+    if (!wallet.account) return;
+    try {
+      const msg = "Hello world!";
+      const msgBytes = new TextEncoder().encode(msg);
+      const result = await wallet.signPersonalMessage({
+        message: msgBytes,
+      });
+      const verifyResult = await wallet.verifySignedMessage(
+        result,
+        wallet.account.publicKey as any
+      );
+      console.log("verify signedMessage", verifyResult);
+      if (!verifyResult) {
+        alert(`signMessage succeed, but verify signedMessage failed`);
+      } else {
+        alert(`signMessage succeed, and verify signedMessage succeed!`);
+      }
+    } catch (e) {
+      console.error("signMessage failed", e);
+      alert("signMessage failed (see response in the console)");
+    }
+  }
+
+  const handleSignTxnAndVerifySignature = async (contractAddress: string) => {
+    const txn = createMintNftTxb(contractAddress);
+    txn.setSender(wallet.account?.address as string);
+    try {
+      const signedTxn = await wallet.signTransaction({
+        transaction: txn,
+      });
+
+      console.log(`Sign and verify txn:`);
+      console.log("--wallet: ", wallet.adapter?.name);
+      console.log("--account: ", wallet.account?.address);
+      const publicKey = wallet.account?.publicKey as any;
+      if (!publicKey) {
+        console.error("no public key provided by wallet");
+        return;
+      }
+      console.log("-- publicKey: ", publicKey);
+      const pubKey = new Ed25519PublicKey(publicKey);
+      console.log("-- signed txnBytes: ", signedTxn.bytes);
+      console.log("-- signed signature: ", signedTxn.signature);
+      const txnBytes = new Uint8Array(Buffer.from(signedTxn.bytes, "base64"));
+      const isValid = await pubKey.verifyTransaction(
+        txnBytes,
+        signedTxn.signature
+      );
+      console.log("-- use pubKey to verify transaction: ", isValid);
+      if (!isValid) {
+        alert(`signTransaction succeed, but verify transaction failed`);
+      } else {
+        alert(`signTransaction succeed, and verify transaction succeed!`);
+      }
+    } catch (e) {
+      console.error("signTransaction failed", e);
+      alert("signTransaction failed (see response in the console)");
+    }
+  };
+
+  const chainName = (chainId: string | undefined) => {
+    switch (chainId) {
+      case SuiChainId.MAIN_NET:
+        return "Mainnet";
+      case SuiChainId.TEST_NET:
+        return "Testnet";
+      case SuiChainId.DEV_NET:
+        return "Devnet";
+      default:
+        return "Unknown";
+    }
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://github.com/suiet/wallet-kit" target="_blank">
+          <img src={suietLogo} className="logo" alt="Suiet logo" />
+        </a>
+      </div>
+      <h1>Vite + Suiet Kit</h1>
+      <div className="card">
+        <ConnectButton
+          onConnectError={(error) => {
+            if (error.code === ErrorCode.WALLET__CONNECT_ERROR__USER_REJECTED) {
+              console.warn(
+                "user rejected the connection to " + error.details?.wallet
+              );
+            } else {
+              console.warn("unknown connect error: ", error);
+            }
+          }}
+        />
+
+        {!wallet.connected ? (
+          <p>Connect DApp with Suiet wallet from now!</p>
+        ) : (
+          <div>
+            <div>
+              <p>current wallet: {wallet.adapter?.name}</p>
+              <p>
+                wallet status:{" "}
+                {wallet.connecting
+                  ? "connecting"
+                  : wallet.connected
+                  ? "connected"
+                  : "disconnected"}
+              </p>
+              <p>wallet address: {wallet.account?.address}</p>
+              <p>current network: {wallet.chain?.name}</p>
+              <p>
+                wallet balance:{" "}
+                {formatSUI(balance ?? 0, {
+                  withAbbr: false,
+                })}{" "}
+                SUI
+              </p>
+              <p>
+                wallet publicKey: {uint8arrayToHex(wallet.account?.publicKey as any)}
+              </p>
+            </div>
+            <div className={"btn-group"} style={{ margin: "8px 0" }}>
+              {nftContractAddr && (
+                <button
+                  onClick={() =>
+                    handleSignAndExecuteTransaction(nftContractAddr, {
+                      isCustomExecution: true,
+                    })
+                  }
+                >
+                  Mint {chainName(wallet.chain?.id)} NFT
+                </button>
+              )}
+              <button onClick={handleSignMsg}>
+                Sign & Verify PersonalMessage
+              </button>
+              {nftContractAddr && (
+                <button
+                  onClick={() =>
+                    handleSignTxnAndVerifySignature(nftContractAddr)
+                  }
+                >
+                  Sign & Verify Transaction
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and Suiet logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import {
+  AllDefaultWallets,
+  defineSlushWallet,
+  WalletProvider,
+} from "@suiet/wallet-kit";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <WalletProvider
+      defaultWallets={[
+        ...AllDefaultWallets,
+        defineSlushWallet({
+          appName: "Suiet Kit Playground",
+        }),
+      ]}
+    >
+      {/* if you want to custiomize you wallet list, please check this doc
+          https://kit.suiet.app/docs/components/WalletProvider#customize-your-wallet-list-on-modal
+       */}
+      <App />
+    </WalletProvider>
+  </React.StrictMode>
+);
